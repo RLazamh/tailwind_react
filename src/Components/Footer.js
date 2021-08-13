129d1521d9b5286998825ccbb7a9ecf35dde9443
RLazamh <roger.laza@epn.edu.ec>
2021-08-13 15:43:00 -0500

    responsive map and logo

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -21,11 +21,15 @@ function Footer(props) {
                 '>
                 <div className='container
                     flex
-                    flex-row
                     w-11/12
                     mx-auto
+                    xs:flex-col
+                    md:flex-col
+                    lg:flex-row
+
+                    
                 '>
-                    <div id='menu-footer' className='footer-box'>
+                    <div id='menu-footer' className='footer-box mt-10'>
                         <h5 className='footer-header'> Menu </h5>
                         <ul className='footer-menu'>
                             <li> <a href="#"> INICIO </a> </li>
@@ -36,16 +40,25 @@ function Footer(props) {
                     </div>
                     <div id="location" className='footer-box'>
                         <h5 className='footer-header'> Donde nos ubicamos? </h5>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.7916578012137!2d-78.49172798635192!3d-0.2094652998471704!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d59a105f4057bb%3A0xb6bddff80696982b!2sFacultad%20de%20Ingenier%C3%ADa%20El%C3%A9ctrica%20y%20Electr%C3%B3nica%20EPN!5e0!3m2!1ses-419!2sec!4v1628879102623!5m2!1ses-419!2sec" width="600" height="450" style={{border:"0;"}} allowfullscreen="" loading="lazy"></iframe>
+                        <iframe className='
+                        w-full 
+                        h-80 
+                        mt-3 
+                        border-4 
+                        border-gray-400 
+                        rounded-md
+                        shadow-md
+                        ' 
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.7916578012137!2d-78.49172798635192!3d-0.2094652998471704!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d59a105f4057bb%3A0xb6bddff80696982b!2sFacultad%20de%20Ingenier%C3%ADa%20El%C3%A9ctrica%20y%20Electr%C3%B3nica%20EPN!5e0!3m2!1ses-419!2sec!4v1628879102623!5m2!1ses-419!2sec" width="600" height="450" style={{border:"0;"}} allowfullscreen="" loading="lazy"></iframe>
 
                     </div>
 
                     <div id='info' className='footer-box'> 
                         <h5 className='footer-header'> Desarrollado con </h5>
-                        <p> <img alt='Desarrollado' src={html5}/> </p>
+                        <p className='mb-5 text-center'> <img className='block mx-auto' alt='Desarrollado' src={html5}/> </p>
 
                         <h5 className='footer-header'> Optimizado para </h5>
-                        <p>
+                        <div className='browsers flex flex-row space-x-5 mb-5' >
                             <a href='#'>
                                <img alt='Firefox' src={firefox}/>
                             </a>
@@ -69,7 +82,7 @@ function Footer(props) {
                             <a href='#'>
                                <img alt='IOS' src={ios}/>
                             </a> 
-                        </p>
+                        </div>
 
                         <h5 className='footer-header'>AUTOR</h5>
                         <p>&copy; Roger Laza </p>
